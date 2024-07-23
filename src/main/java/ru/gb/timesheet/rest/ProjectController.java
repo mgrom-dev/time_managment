f63e5b65c85140f40b9ac7564907f483a367ede9
mgrom-dev <mgrom-dev@ya.ru>
2024-07-23 14:26:13 +0700

    prepare new work

--- src/main/java/ru/gb/timesheet/rest/ProjectController.java
+++ src/main/java/ru/gb/timesheet/rest/ProjectController.java
@@ -3,6 +3,13 @@ package ru.gb.timesheet.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import ru.gb.timesheet.model.Project;
 import ru.gb.timesheet.model.Timesheet;
 import ru.gb.timesheet.service.ProjectService;
@@ -20,11 +27,17 @@ public class ProjectController {
     this.service = service;
   }
 
+  @Operation(summary = "Получить проект по ID", description = "Возвращает проект с указанным ID. Если проект не найден, возвращает 404.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Проект найден", content = @Content(mediaType = "application/json")),
+      @ApiResponse(responseCode = "404", description = "Проект не найден", content = @Content(mediaType = "application/json"))
+  })
+  @Parameter(name = "id", description = "ID проекта", required = true, example = "1")
   @GetMapping("/{id}")
   public ResponseEntity<Project> get(@PathVariable Long id) {
     return service.getById(id)
-      .map(ResponseEntity::ok)
-      .orElseGet(() -> ResponseEntity.notFound().build());
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
 
   @GetMapping("/{id}/timesheets")
